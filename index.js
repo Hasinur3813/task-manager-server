c4a293cbd70a062d8542a33fe5a2ef079ef42074
Hasinur <hasinur.rahman1332@gmail.com>
2025-02-20 23:39:45 +0600

    routes created

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ dotenv.config();
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.0b1vd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,8 +28,12 @@ const client = new MongoClient(uri, {
   },
 });
 
+const db = client.db("task-manager");
+
 async function run() {
   try {
+    const usersCollection = db.collection("users");
+    const taskCollection = db.collection("tasks");
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -33,9 +41,136 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    app.post("/auth/login", async (req, res) => {
+      const user = req.body || {};
+
+      try {
+        // check if the user already exists, if exists then return the user
+        const existingUser = await usersCollection.findOne({
+          email: user?.email,
+        });
+        if (existingUser) {
+          return res.status(200).json({
+            success: true,
+            error: false,
+            type: "existing",
+            message: "User added successfully",
+            data: existingUser,
+          });
+        }
+        // if the user doesn't exist, then add the user to the database
+        const result = await usersCollection.insertOne(user);
+        res.status(201).json({
+          success: true,
+          error: false,
+          type: "new",
+          message: "User added successfully",
+          data: result,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    app.post("/add-task", async (req, res) => {
+      const task = req.body;
+      task.timestamp = new Date(task.timestamp);
+      try {
+        const result = await taskCollection.insertOne(task);
+        res.status(201).json({
+          success: true,
+          error: false,
+          message: "Task added successfully",
+          data: result,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    // get all the task of a user like the format const tasks= [{category:todo, task:[{},{}]}, {category:inProgress, task:[{},{}]}, {category:done, task:[{},{}]}]
+
+    app.get("/tasks/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+
+      try {
+        const tasks = await taskCollection
+          .aggregate([
+            { $match: { user: email } },
+            {
+              $group: {
+                _id: "$category",
+                tasks: { $push: "$$ROOT" },
+              },
+            },
+            {
+              $addFields: {
+                sortOrder: {
+                  $switch: {
+                    branches: [
+                      { case: { $eq: ["$_id", "todo"] }, then: 1 },
+                      { case: { $eq: ["$_id", "inProgress"] }, then: 2 },
+                      { case: { $eq: ["$_id", "done"] }, then: 3 },
+                    ],
+                    default: 4,
+                  },
+                },
+              },
+            },
+            { $sort: { sortOrder: 1 } },
+            {
+              $project: {
+                _id: 0,
+                category: "$_id",
+                tasks: 1,
+              },
+            },
+          ])
+          .toArray();
+
+        res.status(200).json({
+          success: true,
+          error: false,
+          message: "Tasks retrieved successfully",
+          data: tasks,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          success: false,
+          error: true,
+          message: "Failed to retrieve tasks",
+        });
+      }
+    });
+
+    // delete a task
+    app.delete("/tasks/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const result = await taskCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+        res.status(200).json({
+          success: true,
+          error: false,
+          message: "Task deleted successfully",
+          data: result,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          success: false,
+          error: true,
+          message: "Failed to delete task",
+        });
+      }
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
